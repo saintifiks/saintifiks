cd79b8fda8eef4f1e26f199aca72691a029b2433
Saintifiks <saintifiks@gmail.com>
2026-05-30 04:45:19 +0700

    chore: add CSS type declarations to fix TypeScript warnings

---
+++ globals.d.ts
@@ -0,0 +1,22 @@
+// Deklarasi untuk import CSS di TypeScript
+// Menghilangkan warning "Cannot find module" untuk import CSS
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.less' {
+  const content: Record<string, string>;
+  export default content;
+}
